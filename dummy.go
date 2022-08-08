e55b3f7db5cc22ea917638051072a2d4422b3a4a
Wiztim <timothyorlov@gmail.com>
2022-08-07 18:23:44 -0700

    Dummy file uploaded to lambda
    This file has a static input and was modified to work with lambda.

---
+++ dummy.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type OrderInfo struct {
+	TimeStr       string  `json:"dateordered"`   // str representing when order was placed
+	Asin          string  `json:"asin"`          // str represnting item number of product
+	OriginalPrice float32 `json;"originalprice"` // float32 representing the cost the item was bought for
+	//TODO: get current price of item currPrice
+}
+
+//@param: path string - this string represents the filepath of the csv in question
+//@return: Returns an array where each element is an OrderInfo struct containing key item details
+func New() ([]OrderInfo, error) {
+	//let's first open our csv
+	file := strings.NewReader(`Order Date,Order ID,Title,Category,ASIN/ISBN,UNSPSC Code,Website,Release Date,Condition,Seller,Seller Credentials,List Price Per Unit,Purchase Price Per Unit,Quantity,Payment Instrument Type,Purchase Order Number,PO Line Number,Ordering Customer Email,Shipment Date,Shipping Address Name,Shipping Address Street 1,Shipping Address Street 2,Shipping Address City,Shipping Address State,Shipping Address Zip,Order Status,Carrier Name & Tracking Number,Item Subtotal,Item Subtotal Tax,Item Total,Tax Exemption Applied,Tax Exemption Type,Exemption Opt-Out,Buyer Name,Currency,Group Name
+	06/01/20,111-8005663-4090615,"SKYN Original Condoms, 24 Count (Pack of 1)",CONDOM,"B004TTXA7I","53131622",Amazon.com,,new,Amazon.com,,$20.99,$11.17,1,"Discover0179",,,noah.terminello@gmail.com,06/02/20,Noah Terminello,2235 MANDRILL AVE,,VENTURA,CA,93003-7014,Shipped,AMZN_US(TBA050996544001),$11.17,$0.87,$12.04,,,,Noah Terminello,USD`)
+
+	//from the OS file reader, we create a reader for csv
+	r := csv.NewReader(file)
+	r.FieldsPerRecord = 36 // magic number, oops, but this is how many fields are in our CSV
+	orderhist := []OrderInfo{}
+
+	//lets get the headers we care about, noting the first line contains all the field names
+	r.Read()
+
+	//now lets read the actual contents of the csv file
+	//note, each iteration of the for loop reads one record
+	for i := 0; true; i++ {
+		//catch errors
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		//populate our order info fields and create struct
+		s1, s2 := record[0], record[4]
+		f1, err := strconv.ParseFloat(record[12][1:], 32)
+		orderhist = append(orderhist, OrderInfo{s1, s2, float32(f1)})
+	}
+
+	return orderhist, nil
+}
+
+func main() {
+	lambda.Start(New)
+}
