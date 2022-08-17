56eaa9650ea99fcf51a4aa92995186b73b7948bb
owadg <54370773+owadg@users.noreply.github.com>
2022-08-16 18:40:23 -0700

    Update parser.go

--- parser.go
+++ parser.go
@@ -17,14 +17,15 @@ type ItemCategories struct {
 	PriceReduced   []OrderInfo `json:"priceReduced"`
 	PriceUnchanged []OrderInfo `json:"priceUnchanged"`
 	PriceIncreased []OrderInfo `json:"princeIncreased"`
+	Unavailable    []OrderInfo `json:"unavailable"`
 }
 
 type OrderInfo struct {
 	Name          string  `json:"name"`
 	TimeStr       string  `json:"dateOrdered"`   // str representing when order was placed
 	Asin          string  `json:"asin"`          // str represnting item number of product
-	OriginalPrice float64 `json:"originalPrice"` // float32 representing the cost the item was bought for
-	CurrentPrice  float64 `json:"currentPrice"`  //TODO: get current price of item currPrice
+	OriginalPrice float64 `json:"originalPrice"` // float representing the cost the item was bought for
+	CurrentPrice  float64 `json:"currentPrice"`  // curr price
 	PriceDrop     float64 `json:"priceDrop"`     // calced as originalPrice - currentPrice - positive number indicates a drop, negative number indicates an increase
 }
 
@@ -74,15 +75,16 @@ func parseCSV(path string) []OrderInfo {
 		//populate our order info fields and create struct
 		s0, s1, s2 := record[2], record[0], record[4]
 
+		/* deprecated, we don't actually need to do this!!!
 		//let's do a little bit of editing on the name
 		//if there is a comma in the name, it indicates quantities of the package. We want to just remove this part of the name
 		//by taking a substring of the name until we get to the comma
 		if x := strings.Index(s0, ","); x > -1 {
 			s0 = s0[:x]
-		}
+		}*/
 
-		//now we just parse the float
-		f1, err := strconv.ParseFloat(record[12][1:], 64)
+		//now we just parse the float, removing any whitespace from the string
+		f1, err := strconv.ParseFloat(strings.TrimSpace(record[12][1:]), 64)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -94,7 +96,7 @@ func parseCSV(path string) []OrderInfo {
 	return orderhist
 }
 
-//populates currente price in orderhist array
+//populates currente price and price drop in orderhist array
 func getoriginalprice(orderhist *[]OrderInfo) {
 	for i := range *orderhist {
 		//webscraping portion!
@@ -109,23 +111,34 @@ func getoriginalprice(orderhist *[]OrderInfo) {
 
 		//let's look at the HTML body of the response
 		body, err := io.ReadAll(resp.Body)
+
 		strbody := string(body)
+
+		//we first check if the item is unavailable, because if it is unavailable, we will get some NOT okay prices.
+		//this tag should only appear on unavailable items
+		if priceindex := strings.Index(strbody, `<span class="a-color-price a-text-bold">Currently unavailable.</span>`); priceindex != -1 {
+			//unavailable, so let's just skip it
+			//fmt.Println("Sorry, but, ", (*orderhist)[i].Name, " is currently unavailable.")
+			continue
+		}
 
 		//note: the first instance of `<span aria-hidden="true">$` seems to be the actual price of the item before tax, but this
 		//is entirely empiracally decided
-		priceindex := strings.Index(strbody, `<span aria-hidden="true">$`)
+		//NEW IN USE: THIS IS BETTER??? <span class="a-offscreen">$
+		priceindex := strings.Index(strbody, `<span class="a-offscreen">$`)
 
 		//now we need to get the number in this string right after the priceindex
 		//we know this number ends because the span is terminated with "<"
-		//also note `<span aria-hidden="true">$` is 26 chars long
+		//also note `<span class="a-offscreen">$` is 27 chars long
 		var strresult string = ""
-		for strbody[priceindex+26] != '<' {
-			strresult += string(strbody[priceindex+26])
+		for strbody[priceindex+27] != '<' {
+			strresult += string(strbody[priceindex+27])
 			priceindex++
 		}
 		price, err := strconv.ParseFloat(strresult, 64)
 		if err != nil {
 			fmt.Println(err)
+			//fmt.Println("Cannot get price from: ", getUrl((*orderhist)[i].Name, (*orderhist)[i].Asin))
 		}
 
 		//now lets actually modify our array
@@ -133,12 +146,14 @@ func getoriginalprice(orderhist *[]OrderInfo) {
 		(*orderhist)[i].PriceDrop = math.Round(((*orderhist)[i].OriginalPrice-price)*100) / 100
 
 		//done!
+		resp.Body.Close()
+		//fmt.Println("Price obtained from URL: ", getUrl((*orderhist)[i].Name, (*orderhist)[i].Asin), " is: ", price)
 	}
 }
 
 func getUrl(name, asin string) string {
-	//this structure is gotten from analyzing enough URLs
-	itemurl := "https://www.amazon.com/" + name + "/dp/" + asin + "/"
+	//this structure is gotten from analyzing enough URLs, the name part can be literally anything, but this is what I chose
+	itemurl := "https://www.amazon.com/" + name[:15] + "/dp/" + asin + "/"
 	return itemurl
 }
 
@@ -155,10 +170,12 @@ func categorizeItems(orderhist *[]OrderInfo) ItemCategories {
 		switch x := (*orderhist)[i].PriceDrop; {
 		case x > 0: //aka price did drop
 			cat.PriceReduced = append(cat.PriceReduced, (*orderhist)[i])
+		case x == (*orderhist)[i].CurrentPrice: //this means it is unavailable
+			cat.Unavailable = append(cat.Unavailable, (*orderhist)[i])
 		case x == 0: //no drop
-			cat.PriceUnchanged = append(cat.PriceReduced, (*orderhist)[i])
+			cat.PriceUnchanged = append(cat.PriceUnchanged, (*orderhist)[i])
 		case x < 0: //price increased
-			cat.PriceIncreased = append(cat.PriceReduced, (*orderhist)[i])
+			cat.PriceIncreased = append(cat.PriceIncreased, (*orderhist)[i])
 		}
 	}
 
@@ -166,7 +183,7 @@ func categorizeItems(orderhist *[]OrderInfo) ItemCategories {
 }
 
 func main() {
-	b, err := json.Marshal(New("C:\\dev\\GoProjects\\test\\example.csv"))
+	b, err := json.Marshal(New("C:\\dev\\GoProjects\\test\\lupe.csv"))
 	if err != nil {
 		fmt.Println(err)
 	}
