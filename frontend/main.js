48e9b2e441f05a2f061207d77e6e7fda742e6657
asriram31 <44654444+asriram31@users.noreply.github.com>
2022-09-21 19:34:21 -0700

    Final commit version for 09/21

--- frontend/main.js
+++ frontend/main.js
@@ -1,18 +1,33 @@
 const endpoint = "https://www.wiztim.dev/dummy";
 
-function uploadCSVData() {
+
+
+function uploadCSVData(event) {
+
+    event.preventDefault();
+    alert("HI");
     const file = document.getElementById("inpFile").files[0];
 
     var fr = new FileReader();
+    var info;
     fr.readAsText(file);
     fr.onload = function(event) {
-        const info = fr.result;
+        info = fr.result;
     }
+
+    var results = Papa.parse(info);
+
+    console.log(results);
 
     fetch(endpoint, {
         method: "post",
-        body: info
-    }).catch(console.error);
+        body: results
+    })
+        .then(response => {console.log(response);})
+        .catch(console.error);
+    
+
+
 }
 
 function fileValidation(){
@@ -27,18 +42,19 @@ function fileValidation(){
 
     const file = document.getElementById("inpFile").files[0];
     
-    const csvOpener = "Order Date,Order ID,Title,Category,ASIN/ISBN,UNSPSC Code,Website,Release Date,Condition,Seller,Seller Credentials,List Price Per Unit,Purchase Price Per Unit,Quantity,Payment Instrument Type,Purchase Order Number,PO Line Number,Ordering Customer Email,Shipment Date,Shipping Address Name,Shipping Address Street 1,Shipping Address Street 2,Shipping Address City,Shipping Address State,Shipping Address Zip,Order Status,Carrier Name & Tracking Number,Item Subtotal,Item Subtotal Tax,Item Total,Tax Exemption Applied,Tax Exemption Type,Exemption Opt-Out,Buyer Name,Currency,Group Name";
+    // const csvOpener = new RegExp("Order Date,Order ID,Title,Category,ASIN/ISBN,UNSPSC Code,Website,Release Date,Condition,Seller,Seller Credentials,List Price Per Unit,Purchase Price Per Unit,Quantity,Payment Instrument Type,Purchase Order Number,PO Line Number,Ordering Customer Email,Shipment Date,Shipping Address Name,Shipping Address Street 1,Shipping Address Street 2,Shipping Address City,Shipping Address State,Shipping Address Zip,Order Status,Carrier Name & Tracking Number,Item Subtotal,Item Subtotal Tax,Item Total,Tax Exemption Applied,Tax Exemption Type,Exemption Opt-Out,Buyer Name,Currency,Group Name*");
 
-    var fr = new FileReader();
-    fr.readAsText(file);
-    fr.onload = function(event) {
-        const info = fr.result;
-    }
-    if (!csvOpener.exec(info)){
-        alert('Invalid CSV Format - Please use the Items Report Type');
-        filePath = '';
-        return false;
-    }
+    // var fr = new FileReader();
+    // fr.readAsText(file);
+    // var info;
+    // fr.onload = function(event) {
+    //     info = fr.result;
+    // }
+    // if (!csvOpener.exec(info)){
+    //     alert('Invalid CSV Format - Please use the Items Report Type');
+    //     filePath = '';
+    //     return false;
+    // }
     return true;
 
 }
